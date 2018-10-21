e65ac279dffb45b1afb2e6b86837af315afe367c
이상정 <sangjeong@isangjeong-ui-MacBook-Pro.local>
2018-10-22 00:42:49 +0900

    add ga

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,13 @@
 import wrapWithProvider from "./wrap-with-provider";
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-127866309-1");
+
+export const onRouteUpdate = (state, page, pages) => {
+  //   console.log("state", state);
+  //   console.log("page", page);
+  //   console.log("pages", pages);
+  console.log("new pathname", state.location.pathname);
+  ReactGA.pageview(state.location.pathname);
+};
 
 export const wrapRootElement = wrapWithProvider;
